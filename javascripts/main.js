1037a7df55429bbb493e45f5d0ec6d94217502c0
jaewan99 <68039639+jaewan99@users.noreply.github.com>
2020-07-15 21:45:57 +0800

    Add files via upload

---
+++ javascripts/main.js
@@ -0,0 +1,37 @@
+
+let myImage = document.querySelector('img');
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === "images/beach_4k_2-2560x1440.jpg") {
+      myImage.setAttribute ('src','images/man_made_cabin_4k_5k_hd-2560x1440.jpg');
+    } else {
+      myImage.setAttribute ('src',"images/beach_4k_2-2560x1440.jpg");
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+
+
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+}
+
+if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  }
+  
+  myButton.onclick = function() {
+    setUserName();
+}
+
