ba80ccfa9efe2c09aa25af90710860d3546a1384
jaewan99 <68039639+jaewan99@users.noreply.github.com>
2020-09-10 01:41:41 +0900

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,37 +1,20 @@
+function realtimeClock(){
+    var rtClock = new Date();
+    var hours = rtClock.getHours();
+    var minutes = rtClock.getMinutes();
+    var seconds = rtClock.getSeconds();
 
-let myImage = document.querySelector('img');
-myImage.onclick = function() {
-    let mySrc = myImage.getAttribute('src');
-    if(mySrc === "images/beach_4k_2-2560x1440.jpg") {
-      myImage.setAttribute ('src',"images/man_made_cabin_4k_5k_hd-2560x1440.jpg");
-    } else {
-      myImage.setAttribute ('src',"images/beach_4k_2-2560x1440.jpg");
-    }
+    var amPm = (hours < 12) ? "AM" : "PM";
+    hours = (hours > 12) ? hours - 12  : hours;
+
+
+    hours = ("0" + hours).slice(-2);
+    minutes = ("0" + minutes).slice(-2);
+    seconds = ("0" + seconds).slice(-2);
+
+    document.getElementById('clock').innerHTML = hours + " : " + minutes + " : " + seconds + " " + amPm;
+
+    var t = setTimeout(realtimeClock,500);
+
+
 }
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-
-
-function setUserName() {
-    let myName = prompt('Please enter your name.');
-    if(!myName) {
-      setUserName();
-    } else {
-      localStorage.setItem('name', myName);
-      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
-    }
-}
-
-if(!localStorage.getItem('name')) {
-    setUserName();
-  } else {
-    let storedName = localStorage.getItem('name');
-    myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
-  }
-  
-  myButton.onclick = function() {
-    setUserName();
-}
-
